5d63f362487e86a993f55def43d3e053da688551
Lusian Manolov <lusianmanolov@outlook.com>
2019-12-01 16:11:37 +0200

    Added Elevator and ElevatorDoor models

---
+++ Area51Elevator/Area51Elevator/Models/Elevator.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Area51Elevator
+{
+    public sealed class Elevator
+    {
+        private static Random rand = new Random();
+
+        public Elevator()
+        {
+            ElevatorDoor = new ElevatorDoor(Floor.GroundFloor);
+            Semaphore = new Semaphore(1, 1);
+        }
+
+        public ElevatorDoor ElevatorDoor { get; set; }
+        public Semaphore Semaphore { get; set; }
+        public AgentBase Passenger { get; set; }
+
+        public void Enter(AgentBase a, Floor currentFloorNumber, Floor desiredFloorNumber)
+        {
+            Semaphore.WaitOne();
+            Passenger = a;
+            Console.WriteLine($"Agent {Passenger.Name} entered the elevator");
+            SelectFloor(currentFloorNumber);
+        }
+
+        public void SelectFloor(Floor currentFloorNumber)
+        {
+            Floor desiredFloorNumber = (Floor)rand.Next(1, 5);
+
+            if (Passenger != null && Passenger.WantsToGoHome)
+            {
+                desiredFloorNumber = Floor.GroundFloor;
+                Move(currentFloorNumber, desiredFloorNumber);
+            }
+            
+            if (desiredFloorNumber == currentFloorNumber)
+            {
+                SelectFloor(currentFloorNumber);
+            }
+            else
+            {
+                if(Passenger != null)
+                {
+                    Console.WriteLine($"Agent {Passenger.Name} is going to {desiredFloorNumber}");
+                }
+                Move(currentFloorNumber, desiredFloorNumber);
+            }
+        }
+
+        public void Move(Floor currentFloorNumber, Floor desiredFloorNumber)
+        {
+            Thread.Sleep(CalculateMoveTime(currentFloorNumber, desiredFloorNumber));
+            ElevatorDoor.CurrentFloor = desiredFloorNumber;
+            if (Passenger != null)
+            {
+                if (ElevatorDoor.CheckCredentials(Passenger.SecurityLevel, desiredFloorNumber))
+                {
+                    Leave(Passenger);
+                }
+                else
+                {
+                    SelectFloor(ElevatorDoor.CurrentFloor);
+                }
+            }
+        }
+
+        private static int CalculateMoveTime(Floor currentFloorNumber, Floor desiredFloorNumber)
+        {
+            byte greaterFloor = Math.Max((byte)currentFloorNumber, (byte)desiredFloorNumber);
+            byte lesserFloor = Math.Min((byte)currentFloorNumber, (byte)desiredFloorNumber);
+
+            return (greaterFloor - lesserFloor) * 1000;
+        }
+
+        public void Leave(AgentBase a)
+        {
+            Passenger = null;
+            Semaphore.Release();
+            Console.WriteLine($"Agent {a.Name} left the elevator.");
+            a.DoSomeWorkAtCurrentFloor(ElevatorDoor.CurrentFloor);
+        }
+    }
+}
---
+++ Area51Elevator/Area51Elevator/Models/ElevatorDoor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Area51Elevator
+{
+    public sealed class ElevatorDoor
+    {
+        public Floor CurrentFloor { get; set; }
+
+        public ElevatorDoor(Floor initialFloor)
+        {
+            CurrentFloor = initialFloor;
+        }
+
+        public bool CheckCredentials(SecurityLevel sl, Floor floorToAccess)
+        {
+            switch (sl)
+            {
+                case SecurityLevel.Confidential:
+                    if(floorToAccess == Floor.GroundFloor)
+                    {
+                        Console.WriteLine("Access Granted");
+                        return true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Access Denied");
+                        return false;
+                    }
+                case SecurityLevel.Secret:
+                    if (floorToAccess == Floor.GroundFloor || floorToAccess == Floor.NuclearWeaponsFloor)
+                    {
+                        Console.WriteLine("Access Granted");
+                        return true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Access Denied");
+                        return false;
+                    }
+                case SecurityLevel.TopSecret:
+                    Console.WriteLine("Access Granted");
+                    return true;
+                default:
+                    Console.WriteLine("Invalid credentials!");
+                    return false;
+            }
+        }
+    }
+}
